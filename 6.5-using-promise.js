7d80bff144f979fc4b749301002e9ae7204930be
Bidisha Pandit <bidishapandit03@gmail.com>
2023-01-01 20:34:54 +0530

    promisify using util

--- 6.5-using-promise.js
+++ 6.5-using-promise.js
@@ -1,3 +1,4 @@
+//util module
 const { readFile, writeFile } = require("fs");
 const util = require("util");
 const readFilePromise = util.promisify(readFile);
--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-const { readFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFilePromise("./content/first.txt", "utf-8");
+    const second = await readFilePromise("./content/second.txt", "utf-8");
+    await writeFilePromise(
+      "./content/third.txt",
+      ` This is awesome ${first} ${second}`,
+      { flag: "a" }
+    );
     console.log(first, second);
   } catch (error) {
     console.log(error);
