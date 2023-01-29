d7a44b1846dab057427d5ea7f432616c95397f34
Bidisha Pandit <bidishapandit03@gmail.com>
2023-01-29 20:08:52 +0530

    stream on server

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-var http = require("http");
-var fs = require("fs");
 
-http
-  .createServer(function (req, res) {
-    // const text = fs.readFileSync('./content/big.txt', 'utf-8')
-    // res.end(text);
-    //pipe method helps in pushing the readstream to write stream
-    //we read in chunk and then write in chunk as well
-    const fileStream = fs.createReadStream("./content/big.txt");
-    fileStream.on("open", () => {
-      fileStream.pipe(res);
-    });
-    fileStream.on("err", (error) => {
-      res.end(error);
-    });
-  })
-  .listen(5000);
